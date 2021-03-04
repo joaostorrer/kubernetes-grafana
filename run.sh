79f4f81ca3d9961e801b3ef2c8d742308ab178a5
João Filipe Cavichiolo Storrer <jofi_casto@hotmail.com>
2021-03-04 16:31:18 -0300

    Commit inicial

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -f namespace.yaml
+kubectl apply -f persistentvolume.yaml
+kubectl apply -f persistentvolumeclaim.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
